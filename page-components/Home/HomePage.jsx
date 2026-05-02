c8baa4bc7304b3e212866375a6815eadb52a2e47
hemlata6 <hemurajpoot6@gmail.com>
2026-05-02 13:11:12 +0530

    feat: Update start script in package.json to specify port and comment out MarathonGallery in HomePage for cleaner UI

--- page-components/Home/HomePage.jsx
+++ page-components/Home/HomePage.jsx
@@ -44,7 +44,7 @@ export default function HomePage() {
       {/* <AboutSection /> */}
       <CoursesSection />
       <BookStore />
-      <MarathonGallery />
+      {/* <MarathonGallery /> */}
       <StudentGallery />
       <FreeResources />
       <StudentSupportSection />
