00350f5f2528172239cac8b6b5049965e34dfebb
hemlata6 <hemurajpoot6@gmail.com>
2026-05-06 20:32:01 +0530

    Refactor code structure for improved readability and maintainability

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,8 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Favicon */}
-        <link rel="icon" href="/vghub/bghublogo.jpeg" />
-        <link rel="apple-touch-icon" href="/vghub/bghublogo.jpeg" />
+        <link rel="icon" href="/logo.png" />
+        <link rel="apple-touch-icon" href="/logo.png" />
 
         {/* Theme Color */}
         <meta name="theme-color" content="#164e33" />
